6a480acec713921971ea6796e12226a30082f23d
José Domingo Muñoz <josedom24@gmail.com>
2019-03-26 09:45:47 +0100

    requests

--- python_requests/programa_cine_v2.py
+++ python_requests/programa_cine_v2.py
@@ -12,5 +12,13 @@ if r.status_code == 200:
         print(cont,peliculas["title"],"->",peliculas["vote_average"])
         ids.append(peliculas["id"])
         cont+=1
+    indice=int(input("Indica la película de la que quieres obtener información:"))
+    id=ids[indice-1]
+    parametros={"language":"es-ES","api_key":api_key}
+    r=requests.get(URL_BASE+"movie/"+str(id),params=parametros)
+    if r.status_code == 200:
+        datos = r.json()
+        print("Fecha lanzamiento:",datos["release_date"])
+        print(datos["overview"])
 else:
     print("Error en la consulta")
