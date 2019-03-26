f9076df0bd9e05dfb0983b928603248f37738688
José Domingo <josedom24@gmail.com>
2019-03-26 10:45:27 +0100

    requests

--- python_requests/programa_futbol.py
+++ python_requests/programa_futbol.py
@@ -1,1 +1,12 @@
-http://apiclient.resultados-futbol.com/scripts/api/api.php?league=2&key=xxxxx&req=tables&format=json
+import requests
+import os
+api_key=os.getenv("futbol_key")
+URL_BASE="http://apiclient.resultados-futbol.com/scripts/api/api.php"
+parametros={"league":1,"req":"tables","format":"json","key":api_key}
+r=requests.get(URL_BASE,params=parametros)
+if r.status_code == 200:
+    datos = r.json()
+    for equipos in datos["table"]:
+        print(equipos["pos"],equipos["team"],equipos["points"])
+else:
+    print("Error en la consulta")
